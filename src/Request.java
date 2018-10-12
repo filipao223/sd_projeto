c4ec16b980a32aac33fcac31382044683ccc3f2f
João Montenegro <33354398+filipao223@users.noreply.github.com>
2018-10-12 23:30:04 +0100

    Fixed Request.java

--- src/Request.java
+++ src/Request.java
@@ -2,15 +2,6 @@ public class Request {
     //Features
     protected static final int LOGIN             = 1;  //fazer login
     protected static final int MANAGE            = 2;  //gerir artistas, musicas e albuns
-<<<<<<< HEAD
-    protected static final int SEARCH              = 3;  //procurar musicas por album ou artista
-    protected static final int DETAILS             = 4;    //consultar detalhes sobre album e artista
-    protected static final int CRITIQUE           = 5;    //escrever critica a album
-    protected static final int MAKE_EDITOR    = 6;    //dar privilegios de editor a user
-    protected static final int NOTE_EDITOR    = 7;    //notificação de novo editor
-    protected static final int NOTE_NEW_EDIT = 8;    //notificaçao de novo edit
-    protected static final int NOTE_DELIVER   = 9;   //entregar notificação a user previ. off
-=======
     protected static final int SEARCH            = 3;  //procurar musicas por album ou artista
     protected static final int DETAILS           = 4;  //consultar detalhes sobre album e artista
     protected static final int CRITIQUE          = 5;  //escrever critica a album
@@ -18,7 +9,6 @@ public class Request {
     protected static final int NOTE_EDITOR       = 7;  //notificação de novo editor
     protected static final int NOTE_NEW_EDIT     = 8;  //notificaçao de novo edit
     protected static final int NOTE_DELIVER      = 9;  //entregar notificação a user previ. off
->>>>>>> d989179... Request codes
     protected static final int UPLOAD            = 10; //fazer upload de uma musica para o server
     protected static final int SHARE             = 11; //Share de uma musica com users
     protected static final int DOWNLOAD          = 12; //Download de musicas do servidor
